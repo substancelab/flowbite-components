9c0a54c640d102f7fbe311b14bab77abc537ec2a
Jakob Skjerning <jakob@mentalized.net>
2025-04-29 14:03:02 +0200

    Test rendering button sizes

--- test/components/flowbite/button_test.rb
+++ test/components/flowbite/button_test.rb
@@ -9,4 +9,24 @@ class Flowbite::ButtonTest < Minitest::Test
     assert_component_rendered
     assert_selector("button", text: "Click me")
   end
+
+  def test_renders_a_xs_button
+    render_inline(Flowbite::Button.new(label: "Click me", size: :xs))
+    assert_selector("button.text-xs")
+  end
+
+  def test_renders_a_sm_button
+    render_inline(Flowbite::Button.new(label: "Click me", size: :sm))
+    assert_selector("button.text-sm")
+  end
+
+  def test_renders_a_lg_button
+    render_inline(Flowbite::Button.new(label: "Click me", size: :lg))
+    assert_selector("button.text-base.px-5")
+  end
+
+  def test_renders_a_xl_button
+    render_inline(Flowbite::Button.new(label: "Click me", size: :xl))
+    assert_selector("button.text-base.px-6")
+  end
 end
