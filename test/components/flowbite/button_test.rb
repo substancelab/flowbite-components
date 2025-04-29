5da793b148d726231ea47a1e8a271b2f98a365f8
Jakob Skjerning <jakob@mentalized.net>
2025-04-29 16:28:18 +0200

    Test Pill buttons

--- test/components/flowbite/button_test.rb
+++ test/components/flowbite/button_test.rb
@@ -29,4 +29,49 @@ class Flowbite::ButtonTest < Minitest::Test
     render_inline(Flowbite::Button.new(label: "Click me", size: :xl))
     assert_selector("button.text-base.px-6")
   end
+
+  def test_renders_pill_button_in_alternative_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :alternative))
+    assert_selector("button.text-gray-900.bg-white")
+  end
+
+  def test_renders_pill_button_in_dark_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :dark))
+    assert_selector("button.text-white.bg-gray-800")
+  end
+
+  def test_renders_pill_button_in_default_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me"))
+    assert_selector("button.text-white.bg-blue-700")
+  end
+
+  def test_renders_pill_button_in_green_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :green))
+    assert_selector("button.text-white.bg-green-700")
+  end
+
+  def test_renders_pill_button_in_light_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :light))
+    assert_selector("button.text-gray-900.bg-white")
+  end
+
+  def test_renders_pill_button_in_red_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :red))
+    assert_selector("button.text-white.bg-red-700")
+  end
+
+  def test_renders_pill_button_in_purple_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :purple))
+    assert_selector("button.text-white.bg-purple-700")
+  end
+
+  def test_renders_pill_button_in_yellow_style
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", style: :yellow))
+    assert_selector("button.text-white.bg-yellow-400")
+  end
+
+  def test_renders_pill_button_in_different_sizes
+    render_inline(Flowbite::Button::Pill.new(label: "Click me", size: :xs))
+    assert_selector("button.text-white.bg-blue-700.text-xs")
+  end
 end
