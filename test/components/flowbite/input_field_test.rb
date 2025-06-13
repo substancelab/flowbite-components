8cc51cc255c716944d67bf33f92abe5014f678ee
Jakob Skjerning <jakob@mentalized.net>
2025-06-13 15:44:03 +0200

    Add basic tests for inputs

---
+++ test/components/flowbite/input_field_test.rb
@@ -0,0 +1,41 @@
+require "test_helper"
+
+class Flowbite::InputFieldTest < Minitest::Test
+  include ViewComponent::TestHelpers
+
+  class Book
+    include ActiveModel::Model
+
+    attr_accessor :title, :author, :published_at
+
+    validates :title, presence: true
+    validates :author, presence: true
+  end
+
+  def setup
+    @book = Book.new(title: "The Great Gatsby", author: "F. Scott Fitzgerald")
+    @view_context = ActionController::Base.new.view_context
+    @form = ActionView::Helpers::FormBuilder.new(:book, @book, @view_context, {})
+  end
+
+  def test_renders_an_input_element
+    render_inline(Flowbite::InputField.new(form: @form, attribute: :title))
+
+    assert_component_rendered
+    assert_selector("input[type='text'][value='The Great Gatsby']")
+  end
+
+  def test_renders_a_label
+    render_inline(Flowbite::InputField.new(form: @form, attribute: :title))
+
+    assert_component_rendered
+    assert_selector("label[for='book_title']")
+  end
+
+  def test_passes_input_attributes_to_input_element
+    render_inline(Flowbite::InputField.new(form: @form, attribute: :title, input_attributes: {placeholder: "Enter title"}))
+
+    assert_component_rendered
+    assert_selector("input[type='text'][placeholder='Enter title']")
+  end
+end
---
+++ test/components/input/field_test.rb
@@ -0,0 +1,63 @@
+require "test_helper"
+
+class Flowbite::Input::FieldTest < Minitest::Test
+  include ViewComponent::TestHelpers
+
+  class Book
+    include ActiveModel::Model
+
+    attr_accessor :title, :author, :published_at
+
+    validates :title, presence: true
+    validates :author, presence: true
+  end
+
+  def setup
+    @book = Book.new(title: "The Great Gatsby", author: "F. Scott Fitzgerald")
+    @view_context = ActionController::Base.new.view_context
+    @form = ActionView::Helpers::FormBuilder.new(:book, @book, @view_context, {})
+  end
+
+  def test_renders_a_text_input
+    render_inline(Flowbite::Input::Field.new(@form, :title))
+
+    assert_component_rendered
+    assert_selector("input[type='text'][name='book[title]'][value='The Great Gatsby']")
+  end
+
+  def test_renders_with_default_state
+    render_inline(Flowbite::Input::Field.new(@form, :title))
+
+    assert_component_rendered
+    assert_selector("input[name='book[title]'].border-gray-300")
+  end
+
+  def test_renders_in_error_state
+    @book.errors.add(:title, :invalid)
+    render_inline(Flowbite::Input::Field.new(@form, :title))
+
+    assert_component_rendered
+    assert_selector("input[name='book[title]'].border-red-500")
+  end
+
+  def test_renders_with_sm_size
+    render_inline(Flowbite::Input::Field.new(@form, :title, size: :sm))
+
+    assert_component_rendered
+    assert_selector("input[name='book[title]'].p-2.text-xs")
+  end
+
+  def test_renders_with_lg_size
+    render_inline(Flowbite::Input::Field.new(@form, :title, size: :lg))
+
+    assert_component_rendered
+    assert_selector("input[name='book[title]'].text-base")
+  end
+
+  def test_adds_attributes_to_input
+    render_inline(Flowbite::Input::Field.new(@form, :title, input_attributes: {placeholder: "Enter title"}))
+
+    assert_component_rendered
+    assert_selector("input[name='book[title]'][placeholder='Enter title']")
+  end
+end
