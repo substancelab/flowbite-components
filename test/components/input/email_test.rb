0c5dd9fa889750355b2ee90959a3f181b0e410cb
Jakob Skjerning <jakob@mentalized.net>
2025-06-20 10:16:24 +0200

    Add tests for Flowbite::Input::Email

---
+++ test/components/input/email_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+class Flowbite::Input::EmailTest < Minitest::Test
+  include ViewComponent::TestHelpers
+
+  class User
+    include ActiveModel::Model
+
+    attr_accessor :name, :email, :phone
+
+    validates :name, presence: true
+    validates :email, presence: true
+  end
+
+  def setup
+    @user = User.new(name: "John Doe", email: "john@example.com")
+    @view_context = ActionController::Base.new.view_context
+    @form = ActionView::Helpers::FormBuilder.new(:user, @user, @view_context, {})
+  end
+
+  def test_renders_an_email_input
+    render_inline(Flowbite::Input::Email.new(@form, :email))
+
+    assert_selector("input[type='email'][name='user[email]'][value='john@example.com']")
+  end
+
+  def test_renders_with_default_state
+    render_inline(Flowbite::Input::Email.new(@form, :email))
+
+    assert_selector("input[name='user[email]'].border-gray-300")
+  end
+
+  def test_renders_in_error_state
+    @user.errors.add(:email, :invalid)
+    render_inline(Flowbite::Input::Email.new(@form, :email))
+
+    assert_selector("input[name='user[email]'].border-red-500")
+  end
+
+  def test_renders_with_sm_size
+    render_inline(Flowbite::Input::Email.new(@form, :email, size: :sm))
+
+    assert_selector("input[name='user[email]'].p-2.text-xs")
+  end
+
+  def test_renders_with_lg_size
+    render_inline(Flowbite::Input::Email.new(@form, :email, size: :lg))
+
+    assert_selector("input[name='user[email]'].text-base")
+  end
+
+  def test_adds_attributes_to_input
+    render_inline(Flowbite::Input::Email.new(@form, :email, input_attributes: {placeholder: "Enter email address"}))
+
+    assert_selector("input[name='user[email]'][placeholder='Enter email address']")
+  end
+end
