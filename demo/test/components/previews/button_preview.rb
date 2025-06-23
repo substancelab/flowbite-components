67b10df120aec7a3a38d5b473376e3f6222818e1
Jakob Skjerning <jakob@mentalized.net>
2025-06-23 12:50:37 +0200

    Add previews for Flowbite::Button

--- demo/test/components/previews/button_preview.rb
+++ demo/test/components/previews/button_preview.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class ButtonPreview < Lookbook::Preview
+  # @!group Styles
+
+  # Use these default button styles with multiple colors to indicate an action
+  # or link within your website.
+
   def default
     render(Flowbite::Button.new(label: "Click me"))
   end
+
+  def alternative
+    render(Flowbite::Button.new(label: "Click me", style: :alternative))
+  end
+
+  # @!endgroup
 end
