804967077a5b46ca20e6ef5254daf101259eac24
Jakob Skjerning <jakob@mentalized.net>
2025-06-23 11:50:28 +0200

    Mount the Lookbook engine

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    if Rails.env.development?
+      mount Lookbook::Engine, at: "/lookbook"
+    end
+  end
 end
