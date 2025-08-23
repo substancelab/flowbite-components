4119459b9aa9242d2bebdba417df38d32433d58a
Jakob Skjerning <jakob@mentalized.net>
2025-08-23 22:30:23 +0200

    Match ValidationError API to the other components

--- test/components/input/validation_error_test.rb
+++ test/components/input/validation_error_test.rb
@@ -4,19 +4,19 @@ class Flowbite::Input::ValidationErrorTest < Minitest::Test
   include ViewComponent::TestHelpers
 
   def test_renders_validation_error_message
-    render_inline(Flowbite::Input::ValidationError.new(message: "This field is required"))
+    render_inline(Flowbite::Input::ValidationError.new) { "This field is required" }
 
     assert_selector("p.mt-2.text-sm.text-red-600.dark\\:text-red-500", text: "This field is required")
   end
 
   def test_renders_with_empty_message
-    render_inline(Flowbite::Input::ValidationError.new(message: ""))
+    render_inline(Flowbite::Input::ValidationError.new) { "" }
 
     assert_selector("p.mt-2.text-sm.text-red-600.dark\\:text-red-500", text: "")
   end
 
   def test_renders_with_html_escaped_message
-    render_inline(Flowbite::Input::ValidationError.new(message: "Field must be <strong>valid</strong>"))
+    render_inline(Flowbite::Input::ValidationError.new) { "Field must be <strong>valid</strong>" }
 
     assert_selector("p.mt-2.text-sm.text-red-600.dark\\:text-red-500", text: "Field must be <strong>valid</strong>")
     refute_selector("p strong")
@@ -24,7 +24,7 @@ class Flowbite::Input::ValidationErrorTest < Minitest::Test
 
   def test_renders_with_multiline_message
     message = "This field has multiple validation errors:\n- Cannot be blank\n- Must be at least 5 characters"
-    render_inline(Flowbite::Input::ValidationError.new(message: message))
+    render_inline(Flowbite::Input::ValidationError.new) { message }
 
     assert_selector("p.mt-2.text-sm.text-red-600.dark\\:text-red-500", text: message)
   end
