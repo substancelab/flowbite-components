b60017de68bdb804cbc102a0bfe62442f9a051a8
Jakob Skjerning <jakob@mentalized.net>
2025-11-14 09:56:25 +0100

    Follow the same patterns as the other components

--- app/components/flowbite/input/validation_error.rb
+++ app/components/flowbite/input/validation_error.rb
@@ -3,12 +3,36 @@
 module Flowbite
   module Input
     class ValidationError < ViewComponent::Base
+      class << self
+        def classes(state: :default, style: :default)
+          style = styles.fetch(style)
+          style.fetch(state)
+        end
+
+        # rubocop:disable Layout/LineLength
+        def styles
+          {
+            default: Flowbite::Style.new(
+              default: ["mt-2", "text-sm", "text-red-600", "dark:text-red-500"]
+            )
+          }.freeze
+        end
+        # rubocop:enable Layout/LineLength
+      end
+
       def call
-        tag.p(content, class: "mt-2 text-sm text-red-600 dark:text-red-500 #{@class}")
+        tag.p(content, class: classes)
       end
 
       def initialize(class: nil)
-        @class = binding.local_variable_get(:class)
+        @class = Array.wrap(binding.local_variable_get(:class))
+      end
+
+      protected
+
+      # Returns the CSS classes to apply to the validation error
+      def classes
+        self.class.classes + @class
       end
     end
   end
