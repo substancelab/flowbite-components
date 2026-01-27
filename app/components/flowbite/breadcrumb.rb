3fa29e2e2085864e1c59e66a10b42a4817f2ff9b
Jakob Skjerning <jakob@mentalized.net>
2026-01-27 09:52:11 +0100

    Improve Flowbite::Breadcrumb documentation

--- app/components/flowbite/breadcrumb.rb
+++ app/components/flowbite/breadcrumb.rb
@@ -5,6 +5,18 @@ module Flowbite
   #
   # See https://flowbite.com/docs/components/breadcrumb/
   #
+  # Breadcrumbs consist of the following components:
+  #
+  # - {Flowbite::Breadcrumb}: Container for breadcrumb items.
+  # - {Flowbite::BreadcrumbItem}: An individual breadcrumb item.
+  # - {Flowbite::BreadcrumbItem::First}: An individual breadcrumb item with a home icon on it.
+  # - {Flowbite::BreadcrumbSeparator}: Separator between breadcrumb items.
+  # - {Flowbite::BreadcrumbItem::Current}: An invidual breadcrumb item without a link, usually used for the current page in the breadcrumb trail.
+  #
+  # == Slots
+  #
+  # - +items+ ({Flowbite::BreadcrumbItem}): The items of the breadcrumb trail. {Flowbite::Breadcrumb} does not do any special handling for these items, so if you want to render the first item with a home icon, you need to use {Flowbite::BreadcrumbItem::First} for that item. And if you want to render the last item without a link, you need to use {Flowbite::BreadcrumbItem::Current} for that item.
+  #
   # @example Basic usage with BreadcrumbItem components
   #   <%= render Flowbite::Breadcrumb.new do |breadcrumb| %>
   #     <% breadcrumb.with_item do %>
