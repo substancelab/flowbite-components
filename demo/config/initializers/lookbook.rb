282de556ce3e037ef686e2682d4fe68e80776429
Jakob Skjerning <jakob@mentalized.net>
2026-01-30 14:38:52 +0100

    Remove Lookbook customization
    We'll keep Lookbook as close to default as possible, prefering to use
    Rails for the documentation rendering where needed.

--- demo/config/initializers/lookbook.rb
+++ demo/config/initializers/lookbook.rb
@@ -1,7 +1,3 @@
 Rails.application.configure do
   config.view_component.previews.default_layout = "preview"
-
-  Lookbook.after_initialize do |lookbook|
-    Lookbook::PageController.helper(Lookbook::DocHelper)
-  end
 end
