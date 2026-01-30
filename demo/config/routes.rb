c332c55a5970b60463bf3dc684cae6246c6033ca
Jakob Skjerning <jakob@mentalized.net>
2026-01-30 14:47:24 +0100

    Bring the homepage back

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "homepage#show"
 
   namespace :docs do
     resources :components, only: [:show]
