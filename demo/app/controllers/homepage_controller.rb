2da1f6e156964b3ea0e1dac5d5f1a82d3d49dda7
Jakob Skjerning <jakob@mentalized.net>
2026-08-23 14:28:05 +0200

    Feature components on the homepage
    Generated by Claude (Fable) from the following prompt:
    
    > I want to add an overview of the available components to the homepage
    > of the demo site. Stylized SVGs similar to what is on the homepage of
    > https://flowbite-react.com/ or on https://flowbite.com/blocks/.

--- demo/app/controllers/homepage_controller.rb
+++ demo/app/controllers/homepage_controller.rb
@@ -1,4 +1,22 @@
 class HomepageController < ApplicationController
+  FEATURED_COMPONENTS = [
+    {class_name: "Flowbite::Badge", illustration: "badge", name: "Badge"},
+    {class_name: "Flowbite::Breadcrumb", illustration: "breadcrumb", name: "Breadcrumb"},
+    {class_name: "Flowbite::Button", illustration: "button", name: "Button"},
+    {class_name: "Flowbite::Card", illustration: "card", name: "Card"},
+    {class_name: "Flowbite::Input", illustration: "input", name: "Input"},
+    {class_name: "Flowbite::InputField", illustration: "input_field", name: "Input field"},
+    {class_name: "Flowbite::Link", illustration: "link", name: "Link"},
+    {class_name: "Flowbite::Pagination", illustration: "pagination", name: "Pagination"},
+    {class_name: "Flowbite::Sidebar", illustration: "sidebar", name: "Sidebar"},
+    {class_name: "Flowbite::Spinner", illustration: "spinner", name: "Spinner"},
+    {class_name: "Flowbite::Toast", illustration: "toast", name: "Toast"}
+  ].freeze
+
+  helper_method def featured_components
+    FEATURED_COMPONENTS
+  end
+
   def show
   end
 end
